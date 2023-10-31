2a11e212fed1221ae6008763adc62d8f3e75b24a
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-10-31 15:39:39 +0900

    Get bootstrap working with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 //path
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  module: {
+    rules: [
+      {
+        //add css rules
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
 };
