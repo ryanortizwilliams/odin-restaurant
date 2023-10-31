51769b42cd369060eed9f45ac9cde0fc6a9b46b8
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-10-31 16:13:15 +0900

    Removed double import of the javascript bundle

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import "bootstrap";
+import $ from "jquery";
 import "bootstrap/dist/css/bootstrap.css";
+import Popper from "popper.js";
 import "./style.css";
+
+window.jQuery = $;
+window.$ = $;
+window.Popper = Popper;
