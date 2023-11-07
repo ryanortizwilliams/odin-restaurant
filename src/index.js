2be56c827985838acfd767de4572e6c1b6597daf
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-07 16:31:30 +0900

    Enable tabbing using webpack

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ window.jQuery = $;
 window.$ = $;
 window.Popper = Popper;
 
+//Content Div
 document.addEventListener("DOMContentLoaded", () => {
   const content = document.getElementById("content");
 });
@@ -20,3 +21,22 @@ fetch("tab1.html")
   .catch((error) => {
     console.error("Error fetching tab1.html:", error);
   });
+
+//TODO: Create tabbing behaviour
+function changeTab(tabNumber) {
+  content.innerHTML = "";
+  fetch(`tab${tabNumber}.html`)
+    .then((response) => response.text())
+    .then((data) => {
+      content.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error(`Error fetching tab${tabNumber}.html:`, error);
+    });
+}
+//get elements for each nav-link
+const tabs = document.querySelectorAll(".nav-link");
+//put event listener on each one to tab
+tabs.forEach((link, index) => {
+  link.addEventListener("click", changeTab.bind(null, index + 1));
+});
